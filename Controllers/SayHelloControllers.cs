e169e6857f7c11a4cfd5daeea3d16fa0fb19586b
Ulissesortega <Ulisses.ortega@hotmail.com>
2022-10-27 11:54:36 -0700

    upload

--- Controllers/SayHelloControllers.cs
+++ Controllers/SayHelloControllers.cs
@@ -1,7 +1,9 @@
 //Name: Ulises Ortega
 //Date: 10.25.22
 //Project: Say Hello - EndPoit
-//Description: This EndPoint will take the user input and then output in screen Say Hello and the user name.
+//Description: This EndPoint will take the user input in the url and then output in screen Say Hello and the user name.
+//in the example bellow your local host will change, all you need to do is enter your name where it say Your name goes here
+//Example: https://localhost:7242/SayHello/Hello/Your Name Goes Here
 //Peer Review: ARELY MARTINEZ -- THE PROGRAM WORKS WELL. IN POSTMAN I INSERTED MY NAME AFTER THE ROUTE AND IT OUTPUT WHAT'S UP AREALY
 //
 
